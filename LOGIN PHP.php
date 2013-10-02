7d74eab26a439dc339a63c62ffa52b403146fa38
Altz <victory_al7z@yahoo.co.uk>
2013-10-02 15:05:28 +0700

    LOGIN PHP

---
+++ LOGIN PHP.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Login</title>
+</head>
+
+<body<center>
+<form action="proses_d.php" method="post">
+<table width="400" border="0" cellpadding="3" cellspacing="3">
+  <tr>
+    <td colspan="2"><strong>Form Login </strong></td>
+  </tr>
+  <tr>
+    <td width="132">Username</td>
+    <td width="258">:
+    <input name="username" type="text" id="username" /></td>
+  </tr>
+  <tr>
+    <td>Password</td>
+    <td>: 
+    <input name="password" type="password" id="password" /></td>
+  </tr>
+  <tr>
+    <td height="21">&nbsp;</td>
+    <td><input name="Login" type="submit" id="Login" value="Login" /></td>
+  </tr>
+</table></form>
+</center></body>
+</html>
